247840ee2505bd1c94a2922a3c616d21ac4222a1
Playnxbyt <S.Playnx@gmail.com>
2026-05-23 23:53:00 +0530

    Add service worker for asset caching
    Implement service worker for caching assets and offline support.

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "evolve-app-v1";
+const assetsToCache = [
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// Install the service worker and cache files
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch files from cache when offline
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
